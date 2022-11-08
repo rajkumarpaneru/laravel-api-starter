614ead69227bd05a268ece96ca7e122d1d08c4a7
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2022-11-08 19:40:03 +0545

    validation testings for user fields

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,15 +5,22 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:8|confirmed',
+        ]);
+
         User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'password' => $request->password,
+            'password' => Hash::make($request->password),
         ]);
     }
 }
--- tests/Feature/UserRegistrationTest.php
+++ tests/Feature/UserRegistrationTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class UserRegistrationTest extends TestCase
@@ -13,15 +14,128 @@ class UserRegistrationTest extends TestCase
     /** @test */
     public function a_user_can_be_added_by_registration()
     {
-        $this->withoutExceptionHandling();
         $response = $this->post('/api/register', [
             'name' => 'first_user',
             'email' => 'first_user@example.net',
             'password' => 'super-secret',
-            'password_confirmed' => 'super-secret',
+            'password_confirmation' => 'super-secret',
         ]);
 
         $response->assertOk();
         $this->assertCount(1, User::all());
     }
+
+    /** @test */
+    public function a_name_is_required()
+    {
+        $response = $this->post('/api/register', [
+            'name' => '',
+            'email' => 'first_user@example.net',
+            'password' => 'super-secret',
+            'password_confirmation' => 'super-secret',
+        ]);
+
+        $response->assertSessionHasErrors();
+
+    }
+
+    /** @test */
+    public function an_email_is_required()
+    {
+        $response = $this->post('/api/register', [
+            'name' => 'first_user',
+            'email' => '',
+            'password' => 'super-secret',
+            'password_confirmation' => 'super-secret',
+        ]);
+
+        $response->assertSessionHasErrors();
+    }
+
+    /** @test */
+    public function an_email_is_a_valid_email()
+    {
+        $response = $this->post('/api/register', [
+            'name' => 'first_user',
+            'email' => 'first_user.com',
+            'password' => 'super-secret',
+            'password_confirmation' => 'super-secret',
+        ]);
+
+        $response->assertSessionHasErrors();
+    }
+
+    /** @test */
+    public function an_email_is_unique_among_users()
+    {
+        $this->post('/api/register', [
+            'name' => 'first_user',
+            'email' => 'first_user@example.net',
+            'password' => 'super-secret',
+            'password_confirmation' => 'super-secret',
+        ]);
+
+        $response = $this->post('/api/register', [
+            'name' => 'second_user',
+            'email' => 'first_user@example.net',
+            'password' => 'super-secret',
+            'password_confirmation' => 'super-secret',
+        ]);
+
+        $response->assertSessionHasErrors();
+    }
+
+    /** @test */
+    public function a_password_is_required()
+    {
+        $response = $this->post('/api/register', [
+            'name' => 'first_user',
+            'email' => 'first_user@example.net',
+            'password' => '',
+            'password_confirmation' => 'super-secret',
+        ]);
+
+        $response->assertSessionHasErrors();
+    }
+
+    /** @test */
+    public function a_password_is_at_least_eight_character_long()
+    {
+        $response = $this->post('/api/register', [
+            'name' => 'first_user',
+            'email' => 'first_user@example.net',
+            'password' => 'super',
+            'password_confirmation' => 'super',
+        ]);
+
+        $response->assertSessionHasErrors();
+    }
+
+    /** @test */
+    public function a_password_must_be_confirmed()
+    {
+        $response = $this->post('/api/register', [
+            'name' => 'first_user',
+            'email' => 'first_user@example.net',
+            'password' => 'super-secret',
+            'password_confirmation' => 'super',
+        ]);
+
+        $response->assertSessionHasErrors();
+    }
+
+    /** @test */
+    public function hashed_password_is_stored_in_database()
+    {
+        $response = $this->post('/api/register', [
+            'name' => 'first_user',
+            'email' => 'first_user@example.net',
+            'password' => 'super-secret',
+            'password_confirmation' => 'super-secret',
+        ]);
+
+        $hash = Hash::check('super-secret', User::first()->password);
+        $this->assertEquals( true, $hash);
+    }
+
 }
