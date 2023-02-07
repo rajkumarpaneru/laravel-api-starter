058c87ff5ec499c71cfd280660d4b4db66505f0e
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-02-07 22:03:05 +0545

    update verify_email content

--- resources/views/email/verify_email.blade.php
+++ resources/views/email/verify_email.blade.php
@@ -1,13 +1,45 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Verify your email</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <style>
+        /* Add your custom styles here */
+        body {
+            font-family: Arial, sans-serif;
+            padding: 20px;
+        }
+        h1 {
+            font-size: 24px;
+            color: #333;
+            text-align: center;
+            margin-bottom: 20px;
+        }
+        .content {
+            background-color: #fafafa;
+            padding: 20px;
+            border-radius: 10px;
+        }
+        .btn {
+            background-color: #333;
+            color: #fff;
+            padding: 10px 20px;
+            border-radius: 25px;
+            text-decoration: none;
+            display: inline-block;
+            margin-top: 20px;
+        }
+    </style>
 </head>
 <body>
-
+<h1>Email Verification</h1>
+<div class="content">
+    <p>
+        Hello, thank you for registering with our site. To complete your registration, please verify your email by clicking the
+        button below.
+    </p>
+    <a href="#" class="btn">Verify Email</a>
+</div>
 </body>
 </html>
